2c5d6c3d60dab9dfb0e51f1d852d654954c60599
liuziyang <280911198@qq.com>
2018-08-04 13:19:56 +0800

    工厂模式

---
+++ src/com/singleton/Singleton.java
@@ -0,0 +1,19 @@
+package com.singleton;
+
+/**
+ * 单例模式（懒汉式线程不安全）
+ */
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+}
