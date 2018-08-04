db0871eed0b9ffc110defb665c266fe03f0b815e
liuziyang <280911198@qq.com>
2018-08-04 13:02:10 +0800

    工厂模式

--- src/com/singleton/SingleObject.java
+++ src/com/singleton/SingleObject.java
@@ -7,5 +7,15 @@ package com.singleton;
  * @date 2018-8-4
  */
 public class SingleObject {
-    
+    private static SingleObject instance = new SingleObject();
+
+    private SingleObject(){};
+
+    public static SingleObject getInstance(){
+        return instance;
+    }
+
+    public void showMessage(){
+        System.out.println("hello singleton");
+    }
 }
