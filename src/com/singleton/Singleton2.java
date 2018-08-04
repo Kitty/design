72bedb2665f6573e211173f07adedad36912df51
liuziyang <280911198@qq.com>
2018-08-04 13:26:04 +0800

    工厂模式

--- src/com/singleton/Singleton2.java
+++ src/com/singleton/Singleton2.java
@@ -1,4 +1,21 @@
 package com.singleton;
 
+/**
+ * 单例模式（懒汉式线程安全）
+ *
+ * @author lzy
+ * @date 2018-8-4
+ */
 public class Singleton2 {
+    private static Singleton2 instance;
+
+    private Singleton2() {
+    }
+
+    public static synchronized Singleton2 getInstance() {
+        if (instance == null) {
+            instance = new Singleton2();
+        }
+        return instance;
+    }
 }
