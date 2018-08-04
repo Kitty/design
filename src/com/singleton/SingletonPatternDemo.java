fefb4e8b03b6142f8b202951911716a6d5fabd5b
liuziyang <280911198@qq.com>
2018-08-04 13:05:45 +0800

    工厂模式

---
+++ src/com/singleton/SingletonPatternDemo.java
@@ -0,0 +1,15 @@
+package com.singleton;
+
+/**
+ * 单例模式
+ *
+ * @author lzy
+ * @date 2018-8-4
+ */
+public class SingletonPatternDemo {
+    public static void main(String[]args){
+        SingleObject object = SingleObject.getInstance();
+
+        object.showMessage();
+    }
+}
