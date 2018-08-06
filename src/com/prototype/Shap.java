315dae258b8d9317411f3f1ec4e5b106ac2f400e
liuziyang <280911198@qq.com>
2018-08-06 22:06:42 +0800

    原型模式

--- src/com/prototype/Shap.java
+++ src/com/prototype/Shap.java
@@ -7,4 +7,31 @@ package com.prototype;
  * @date 2018-8-6
  */
 public abstract class Shap implements Cloneable {
+    private String id;
+    private String type;
+
+    abstract void draw();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        Object clone = null;
+        clone = super.clone();
+        return clone();
+    }
 }
