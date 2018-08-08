1ebd686edc58411dd12277e323bf0d150af5571e
liuziyang <280911198@qq.com>
2018-08-08 09:56:40 +0800

    适配器模式

--- src/com/adapter/MediaAdapter.java
+++ src/com/adapter/MediaAdapter.java
@@ -1,5 +1,10 @@
 package com.adapter;
-
+/**
+ * 适配器模式
+ *
+ * @author lzy
+ * @date 2018-8-7
+ */
 public class MediaAdapter implements MediaPlayer {
     AdvancedMediaPlayer advancedMediaPlayer;
 
