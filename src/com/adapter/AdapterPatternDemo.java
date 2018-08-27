ccd5eca266cdd2ba5861991c23b01c9b0e65adab
liuziyang <280911198@qq.com>
2018-08-27 23:01:04 +0800

    工厂模式

--- src/com/adapter/AdapterPatternDemo.java
+++ src/com/adapter/AdapterPatternDemo.java
@@ -8,6 +8,7 @@ package com.adapter;
  */
 public class AdapterPatternDemo {
     public static void main(String[] args) {
+        //进行测试
         AudioPlayer audioPlayer = new AudioPlayer();
         audioPlayer.play("mp3", "beyond the horizon.mp3");
         audioPlayer.play("mp4", "alone.mp4");
